535287e6db9f5218e36d21b4182322627e2833cd
PedroVictor022 <pedro20002victor@gmail.com>
2023-05-08 22:37:32 -0300

    ex3

---
+++ src/estrutura_de_dados/exercicios/ex3.java
@@ -0,0 +1,11 @@
+package estrutura_de_dados.exercicios;
+
+public class ex3 {
+    static public void main(String[] args) {
+        for(int i = 0;i < 100;i++) {
+            if(i % 3 == 0) {
+                System.out.println("Este numero " + i + " e divisivel por 3");
+            }
+        }
+    }
+}
