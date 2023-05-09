a6ab306c4cc3e412de562e9523d3bdd0b1c29ac4
PedroVictor022 <pedro20002victor@gmail.com>
2023-05-09 14:39:57 -0300

    objetos por referencia

--- src/poo/Conta.java
+++ src/poo/Conta.java
@@ -6,6 +6,10 @@ class Conta {
     double saldo;
 
 //    void informa que nenhuma informacao sera enviada a quem chamou este metodo;
+
+    void infoConta() {
+        System.out.println("Nome: " +this.titular + "\n" + "Saldo: " + this.saldo);
+    }
     void sacar(double quantidade) {
         double novoSaldo = this.saldo -= quantidade;
         if(quantidade > saldo) {
@@ -17,10 +21,30 @@ class Conta {
         }
     }
 
-    void depositar(double quantidade) {
-        double novoSaldo = this.saldo += quantidade;
-        System.out.println("Deposito realizado com sucesso!");
-        System.out.println("NOVO SALDO DE : R$" + novoSaldo);
+    boolean depositar(double quantidade) {
+        if(quantidade < 0){
+            System.out.println("Este valor nao e aceito");
+            return false;
+        } else {
+            double novoSaldo = this.saldo += quantidade;
+            System.out.println("Deposito realizado com sucesso!");
+            System.out.println("NOVO SALDO DE : R$" + novoSaldo);
+            return true;
+        }
+    }
+
+    boolean transferePara(Conta destino, double quantidade) {
+        if(quantidade > this.saldo) {
+            System.out.println("Nao foi possivel transferir, saldo insuficiente!");
+            return false;
+        } else {
+            double valor = this.saldo - quantidade;
+            destino.saldo = destino.saldo + quantidade;
+            System.out.println("Transacao realizada com sucesso!");
+            return true;
+        }
+
     }
 
 }
+
--- src/poo/main.java
+++ src/poo/main.java
@@ -2,15 +2,20 @@ package poo;
 
 public class main {
     public static void main(String[] args) {
-        Conta minhaConta;
+        Conta minhaConta, fulano;
         minhaConta = new Conta();
+        fulano = new Conta();
 
         minhaConta.numero_conta = 123123;
         minhaConta.titular = "Pedro Victor";
         minhaConta.saldo = 200;
-
+        minhaConta.transferePara(fulano, 50);
         minhaConta.depositar(500);
-
         minhaConta.sacar(50);
+
+        fulano.titular = "Fulano da Silva";
+
+        fulano.infoConta();
+
     }
 }
