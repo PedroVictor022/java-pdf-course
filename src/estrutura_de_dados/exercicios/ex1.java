b0468d271895796637142641513368c7705d1fc1
PedroVictor022 <pedro20002victor@gmail.com>
2023-05-08 22:30:48 -0300

    ex1

---
+++ src/estrutura_de_dados/exercicios/ex1.java
@@ -0,0 +1,10 @@
+package estrutura_de_dados.exercicios;
+
+public class ex1 {
+    static public void main(String[] args) {
+        int x = 300;
+        for(int i =150; i < x;i++) {
+            System.out.println(i);
+        }
+    }
+}
