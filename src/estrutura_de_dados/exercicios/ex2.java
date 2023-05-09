277c2077b0ad7e96051ad084d0240494f5be064d
PedroVictor022 <pedro20002victor@gmail.com>
2023-05-08 22:33:49 -0300

    ex2

---
+++ src/estrutura_de_dados/exercicios/ex2.java
@@ -0,0 +1,10 @@
+package estrutura_de_dados.exercicios;
+
+public class ex2 {
+    static public void main(String[] args) {
+        int soma = 0;
+        for(int i = 0; i < 1000;i++) {
+            System.out.println(soma + i);
+        }
+    }
+}
