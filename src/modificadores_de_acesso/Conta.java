3cd66cf7c6e902414a22078fd27dfd5845233203
PedroVictor022 <pedro20002victor@gmail.com>
2023-05-10 22:36:06 -0300

    finish cap

--- src/modificadores_de_acesso/Conta.java
+++ src/modificadores_de_acesso/Conta.java
@@ -4,6 +4,13 @@ public class Conta {
     String titular;
     int numero;
     private double saldo;
+    private int totalDeContas;
+
+//    TODA VEZ QUE UM OBJETO FOR INSTANCIADO O METODO ABAIXO VAI SER CRIADO
+    Conta() {
+        System.out.println("Iniciando uma conta");
+        this.totalDeContas = this.totalDeContas + 1;
+    }
     void saca(double valor) {
         if(valor > this.saldo) {
             System.out.println("Nao posso sacar um valor maior que o saldo");
--- src/modificadores_de_acesso/main.java
+++ src/modificadores_de_acesso/main.java
@@ -3,6 +3,7 @@ package modificadores_de_acesso;
 public class main {
     public static void main(String[] args) {
         Conta c1 = new Conta();
+        Conta c2 = new Conta();
         c1.numero = 0;
         c1.titular = "Pedro";
         c1.setSaldo(9000);
